0b0392658617b4f674752f383e406772d79e14fc
CarlosOMG57 <carlosnekoomg@gmail.com>
2025-01-10 15:22:08 -0600

    Actividad 3

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.querySelector('header').addEventListener('mouseenter', function() {
+    this.style.backgroundColor = '#1e7eb2';
+  });
+  
+  document.querySelector('header').addEventListener('mouseleave', function() {
+    this.style.backgroundColor = '#2d9cdb';
+  });
+  
